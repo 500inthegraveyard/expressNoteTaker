2adffba8bdd2c6cc925b9dc704d51c1686ac1431
500inthegraveyard <hoostasr@gmail.com>
2020-09-02 11:09:29 -0500

    push with readme and new file

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # expressNoteTaker
+
+## Link to Live Application
+
+
+## Application Purpose
+This
+
+## Start Application
+
+In the project directory, you can run:
+### `npm i`
+and then run
+### `npm start`
+
+Runs the app in the development mode.<br />
+Open [http://localhost:3000]
+
+
+
+## Screenshot
+![Alt text](Screenshot.JPG?raw=true "screenshot")
--- server.js
+++ server.js
@@ -1,85 +1,17 @@
+const express = require("express");
+const apiRoutes = require("./routes/apiRoutes");
+const htmlRoutes = require("./routes/htmlRoutes");
 
-var express = require("express");
-var path = require("path");
-const { domainToASCII } = require("url");
-var dbjson = require("./db/db.json");
-// const { fstat } = require("fs");
-console.log("org dbjson", dbjson);
-var fs = require('fs');
+// Initialize the app and create a port
+const app = express();
+const PORT = process.env.PORT || 3000;
 
-var app = express();
-var PORT = process.env.PORT|| 3000 ;
-
-app.use(express.urlencoded({ extended: true }));
+// Set up body parsing, static, and route middleware
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
 
-
-
-
-
-app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "public/index.html"));
-});
-
-app.get("/notes", function(req, res) {
-  res.sendFile(path.join(__dirname, "public/notes.html"));
-});
-
-
-app.get("/api/notes", function(req, res) {
-    res.json(dbjson);
-});
-
-
-app.post("/api/notes", function(req, res) {
-    
-    var newNote = req.body;
-    dbjson.push(newNote);
-    fs.writeFile("./db/db.json",JSON.stringify(dbjson), function(err){
-        if (err) throw err;
-        console.log('Saved!');
-    
-    })
-    
-    console.log("new dbjson", dbjson);
-
-
-  });
-
-//Below was where I tried to delet the new notes with id's but I couldnt get it to work:
-
-// app.delete("/api/notes/:id", function(req, res) {
-//   try {
-//     notesData = fs.readFileSync("./develop/db/db.json", "utf8");
-//     notesData = JSON.parse(notesData);
-//     notesData = notesData.filter(function(note) {
-//       return note.id != req.params.id;
-//     });
-//     notesData = JSON.stringify(notesData);
-//     fs.writeFile("./develop/db/db.json", notesData, "utf8", function(err) {
-//       if (err) throw err;
-//     });
-//     res.send(JSON.parse(notesData));
-//   } catch (err) {
-//     throw err;
-//     console.log(err);
-//   }
-// });
-  
-
-app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
-  });
-  
-
-
-
-
-
-
-
-
-
-
-
+// Start the server on the port
+app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
